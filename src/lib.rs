19b9aeb53339198a24566b3448768ffd4c49fa88
Technically Inclined <technicallyinclined@proton.me>
2022-10-10 17:27:38 -0400

    fixing cargo test error

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@ async fn health_check() -> HttpResponse {
 
 #[derive(serde::Deserialize)]
 struct FormData {
-     email: String,
-     name: String,
+    email: String,
+    name: String,
 }
 
 async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
